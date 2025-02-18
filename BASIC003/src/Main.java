dc355a769e5ccedfb48431a3443c6faa1ad8bce4
Khanhptitnhe <168113992+Khanhptitnhe@users.noreply.github.com>
2025-02-18 20:28:19 +0700

    Update Main.java

--- BASIC003/src/Main.java
+++ BASIC003/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
+    public static void isLeapYear(Scanner sc){
+        int y = sc.nextInt();
+        if((y %4==0 && y % 100 != 0) || (y %400 ==0) ){
+            System.out.println(y + " : Leap-year");
+        }else{
+            System.out.println(y + " : Non Leap-year");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            isLeapYear(sc);
+            t--;
+        }
     }
 }
